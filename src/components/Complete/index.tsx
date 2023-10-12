b8bd88ebd73d4c7069dce2e67434bf5281e5018c
Lee-Seungje <moondgod@gmail.com>
2023-10-12 14:49:56 +0900

    Update complet size

--- src/components/Complete/index.tsx
+++ src/components/Complete/index.tsx
@@ -2,10 +2,26 @@ import styled from "@emotion/styled";
 import { Animals, Logo } from "@/assets";
 import Image from "next/image";
 import { PhotoBoothStep } from "@/types";
+import Button from "../common/Button";
 
-const Complete = () => {
+interface CompleteProps {
+  nextStep: PhotoBoothStep;
+  prevStep: PhotoBoothStep;
+}
+
+const Complete: React.FC<CompleteProps> = ({ nextStep, prevStep }) => {
+  function setPhotoBoothStep(prevStep: any): void {
+    throw new Error("Function not implemented.");
+  }
+
   return (
     <BackGround>
+      <Header>
+        <Description>지니포토가 인쇄되고 있어요</Description>
+        <Button icon="NEXT" onClick={() => setPhotoBoothStep(nextStep)}>
+          메인으로
+        </Button>
+      </Header>
       <AnimalsWrapper>
         <Image src={Animals} fill alt="" />
       </AnimalsWrapper>
@@ -22,13 +38,10 @@ const Complete = () => {
 export default Complete;
 
 const BackGround = styled.div`
-  width: 1871px;
+  width: 1671px;
   height: 1000px;
-  margin: 20px 0 0 22px;
+  margin: 22px 0 0 22px;
   background-color: white;
-  display: flex;
-  align-items: center;
-  padding-left: 389px;
 `;
 
 const ImageWrapper = styled.div`
@@ -41,22 +54,22 @@ const ImageWrapper = styled.div`
 `;
 
 const AnimalsWrapper = styled(ImageWrapper)`
-  width: 605px;
-  height: 605px;
-  top: 265px;
-  left: 345px;
+  width: 505px;
+  height: 505px;
+  top: 225px;
+  left: 305px;
 `;
 
 const LogoWrapper = styled(ImageWrapper)`
   position: relative;
-  width: 477px;
-  height: 343px;
+  width: 457px;
+  height: 333px;
 `;
 
 const Contents = styled.div`
   position: absolute;
-  top: 359px;
-  right: 277px;
+  top: 279px;
+  right: 287px;
   width: 500px;
   display: flex;
   align-items: center;
@@ -74,4 +87,20 @@ const CompleteText = styled.span`
   font-weight: 300;
   line-height: normal;
   letter-spacing: -4.8px;
+`;
+
+const Header = styled.div`
+  width: calc(100vw - 100px);
+  display: flex;
+  padding: 45px 46px 32px 656px;
+  justify-content: space-between;
+  margin-left: 20px;
+`;
+
+const Description = styled.p`
+  color: #000;
+  font-size: 38px;
+  font-weight: 400;
+  letter-spacing: -2.4px;
+  font-family: "GmarketSansMedium";
 `;
