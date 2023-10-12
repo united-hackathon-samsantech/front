78ab33a826fca3476865b6107c17c055f0376782
Lee-Seungje <moondgod@gmail.com>
2023-10-12 16:53:17 +0900

    Add isLoad

--- src/components/FinalSelect/index.tsx
+++ src/components/FinalSelect/index.tsx
@@ -12,6 +12,7 @@ import Button from "../common/Button";
 import { Configuration, OpenAIApi } from "openai";
 import { useTranslatedTextStore } from "@/store/translatedText";
 import axios from "axios";
+import { Loading } from "@/components";
 
 const colors = [
   {
@@ -44,6 +45,7 @@ const FinalSelect = ({ nextStep, prevStep }: FinalSelectProps) => {
   const openai = new OpenAIApi(configuration);
 
   const generateImage = async () => {
+    setIsLoad(false);
     const res = await openai.createImage({
       prompt: inputValue[0],
       n: 3,
@@ -52,6 +54,7 @@ const FinalSelect = ({ nextStep, prevStep }: FinalSelectProps) => {
     let imageArr = [];
     imageArr = res.data.data.map((v) => v.url ?? "");
     setResult(imageArr);
+    setIsLoad(true);
   };
 
   const getAPIKey = async () => {
@@ -70,6 +73,8 @@ const FinalSelect = ({ nextStep, prevStep }: FinalSelectProps) => {
   const [selectedFrame, setSelectedFrame] = useState<number>(0);
   const [result, setResult] = useState<string[]>([]);
 
+  const [isLoad, setIsLoad] = useState<boolean>(false);
+
   const setPhotoBoothStep = useSetPhotoBoothStepStore();
 
   useEffect(() => {
@@ -78,89 +83,96 @@ const FinalSelect = ({ nextStep, prevStep }: FinalSelectProps) => {
   }, []);
 
   return (
-    <BackGround>
-      <Header>
-        <Button icon="PREV" onClick={() => setPhotoBoothStep(prevStep)}>
-          돌아가기
-        </Button>
-        <Description>프레임 생성을 위해 태그를 선택해주세요</Description>
-        <Button icon="NEXT" onClick={() => setPhotoBoothStep(nextStep)}>
-          인쇄하기
-        </Button>
-      </Header>
-      <Main>
-        <Frame
-          css={css`
-            ${selectedFrame < 3
-              ? `background-image: url(${result[selectedFrame]});`
-              : "background: " + (selectedFrame === 3 ? "black;" : "white;  ")}
-            background-size: cover;
-            ${selectedFrame === 4 && "border: solid 1px black"};
-          `}
-        >
-          <ImageContainer>
-            {images.map((image) => (
-              <Image
-                alt={image}
-                src={image}
-                width={163}
-                height={221}
-                unoptimized
-              />
-            ))}
-          </ImageContainer>
-          <Image src={SmallLogo} alt="logo" width={159} height={85} />
-        </Frame>
-        <Contents>
-          <Title>프레임 선택</Title>
-          <Section>
-            <SectionTitle>AI 지니가 생성한 프레임</SectionTitle>
-            <FrameImages>
-              {result.map((image, i) => (
-                <FrameImage
-                  onClick={() => setSelectedFrame(i)}
-                  css={
-                    i === selectedFrame &&
-                    css`
-                      box-shadow: 15px 15px 13px #666;
-                      position: relative;
-                      bottom: 10px;
-                      transition: ease-in-out 0.3s;
-                    `
-                  }
-                >
-                  <Image unoptimized src={image} alt="FrameImage" fill />
-                </FrameImage>
-              ))}
-            </FrameImages>
-            <ReCreateWrapper>
-              <Button icon="NEXT" onClick={generateImage}>
-                재생성하기
-              </Button>
-            </ReCreateWrapper>
-          </Section>
-          <Section>
-            <SectionTitle>일반 프레임</SectionTitle>
-            <Colors>
-              {colors.map((color, i) => (
-                <Color
-                  onClick={() => setSelectedFrame(i + 3)}
-                  isSelect={i + 3 === selectedFrame}
-                  css={
-                    color.name === "white" &&
-                    css`
-                      border: black 1px solid;
-                    `
-                  }
-                  color={color.color}
-                />
-              ))}
-            </Colors>
-          </Section>
-        </Contents>
-      </Main>
-      <Logo />
-    </BackGround>
+    <>
+      {isLoad ? (
+        <BackGround>
+          <Header>
+            <Button icon="PREV" onClick={() => setPhotoBoothStep(prevStep)}>
+              돌아가기
+            </Button>
+            <Description>프레임 생성을 위해 태그를 선택해주세요</Description>
+            <Button icon="NEXT" onClick={() => setPhotoBoothStep(nextStep)}>
+              인쇄하기
+            </Button>
+          </Header>
+          <Main>
+            <Frame
+              css={css`
+                ${selectedFrame < 3
+                  ? `background-image: url(${result[selectedFrame]});`
+                  : "background: " +
+                    (selectedFrame === 3 ? "black;" : "white;  ")}
+                background-size: cover;
+                ${selectedFrame === 4 && "border: solid 1px black"};
+              `}
+            >
+              <ImageContainer>
+                {images.map((image) => (
+                  <Image
+                    alt={image}
+                    src={image}
+                    width={163}
+                    height={221}
+                    unoptimized
+                  />
+                ))}
+              </ImageContainer>
+              <Image src={SmallLogo} alt="logo" width={159} height={85} />
+            </Frame>
+            <Contents>
+              <Title>프레임 선택</Title>
+              <Section>
+                <SectionTitle>AI 지니가 생성한 프레임</SectionTitle>
+                <FrameImages>
+                  {result.map((image, i) => (
+                    <FrameImage
+                      onClick={() => setSelectedFrame(i)}
+                      css={
+                        i === selectedFrame &&
+                        css`
+                          box-shadow: 15px 15px 13px #666;
+                          position: relative;
+                          bottom: 10px;
+                          transition: ease-in-out 0.3s;
+                        `
+                      }
+                    >
+                      <Image unoptimized src={image} alt="FrameImage" fill />
+                    </FrameImage>
+                  ))}
+                </FrameImages>
+                <ReCreateWrapper>
+                  <Button icon="NEXT" onClick={generateImage}>
+                    재생성하기
+                  </Button>
+                </ReCreateWrapper>
+              </Section>
+              <Section>
+                <SectionTitle>일반 프레임</SectionTitle>
+                <Colors>
+                  {colors.map((color, i) => (
+                    <Color
+                      onClick={() => setSelectedFrame(i + 3)}
+                      isSelect={i + 3 === selectedFrame}
+                      css={
+                        color.name === "white" &&
+                        css`
+                          border: black 1px solid;
+                        `
+                      }
+                      color={color.color}
+                    />
+                  ))}
+                </Colors>
+              </Section>
+            </Contents>
+          </Main>
+          <Logo />
+        </BackGround>
+      ) : (
+        <Loading />
+      )}
+    </>
   );
 };
 
